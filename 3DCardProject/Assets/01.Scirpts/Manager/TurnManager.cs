b4e6d9d97fbe6b14025c0426096573e534abfc3a
Kimbeomju <kbj52264291@gmail.com>
2022-05-29 17:34:21 +0900

    구체화 취소

--- 3DCardProject/Assets/01.Scirpts/Manager/TurnManager.cs
+++ 3DCardProject/Assets/01.Scirpts/Manager/TurnManager.cs
@@ -14,19 +14,19 @@ public enum TurnType
 public class TurnManager : Singleton<TurnManager>
 {
     [SerializeField] private TurnType type;
-    [SerializeField] private CanvasGroup turnPanel;
-    [SerializeField] private TMP_Text turnText;
+    //[SerializeField] private CanvasGroup turnPanel;
+    //[SerializeField] private TMP_Text turnText;
 
     // 턴 바꿈 
     public static void ChangeTurn(TurnType _type)
     {
         Instance.type = _type;
-        if(_type == TurnType.Player)
-            Instance.turnText.text = "플레이어 턴";
-        else
-            Instance.turnText.text = "적 턴";
+        //if(_type == TurnType.Player)
+        //    Instance.turnText.text = "플레이어 턴";
+        //else
+        //    Instance.turnText.text = "적 턴";
 
-        Instance.turnPanel.DOFade(1, 0.5f).OnComplete(() => Instance.turnPanel.DOFade(0, 0.5f));
+        //Instance.turnPanel.DOFade(1, 0.5f).OnComplete(() => Instance.turnPanel.DOFade(0, 0.5f));
     }
 
     // 현재 타입 반환 
