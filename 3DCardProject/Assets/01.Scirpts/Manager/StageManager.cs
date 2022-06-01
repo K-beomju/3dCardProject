b8c9c5fca065dca88b933e5f19dfc64100692ad8
Kimbeomju <kbj52264291@gmail.com>
2022-06-01 22:59:57 +0900

    스테이지 매니저 수정

--- 3DCardProject/Assets/01.Scirpts/Manager/StageManager.cs
+++ 3DCardProject/Assets/01.Scirpts/Manager/StageManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class StageManager : Singleton<StageManager>
 {
-    public int stageNum = 0;
+    [Header("Stages")]
+    public static int targetStage = 0;
+    public int debug_targetStage = 0; 
     [SerializeField] private StageArraySO stageSO;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (debug_targetStage != -1)
+        {
+            targetStage = debug_targetStage;
+        }
+    }
 
     // TODO: 플레이어도 스테이지도 공유해서 씀 개인작업이 다 끝나면 Utills 스크립트로 이전?
     public static List<Item> SetStageBuffer(List<Item> itemList)
     {
-        for(int i = 0; i < Instance.stageSO.stages[Instance.stageNum].StageData.Length; i++)
+        for(int i = 0; i < Instance.stageSO.stages[targetStage].StageData.Length; i++)
         {
-            Item item = Instance.stageSO.stages[Instance.stageNum].StageData[i].itemSo.item;
+            Item item = Instance.stageSO.stages[targetStage].StageData[i].itemSo.item;
             for (int j = 0; j < item.count; j++)
                 itemList.Add(item);
         }
