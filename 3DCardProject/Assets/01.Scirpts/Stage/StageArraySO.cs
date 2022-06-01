5e8ee32ddac9cc02a1ffd411053e6dd495ecfdc9
Kimbeomju <kbj52264291@gmail.com>
2022-06-01 22:37:01 +0900

    StageArrayList 추가

---
+++ 3DCardProject/Assets/01.Scirpts/Stage/StageArraySO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class StageData
+{
+    public ItemSO item;
+    public Vector2Int vector2Int;
+}
+
+
+[CreateAssetMenu(fileName = "StageArraySO", menuName = "Scriptable Object/StageArraySO")]
+public class StageArraySO : ScriptableObject
+{
+    public List<StageSO> stages;
+}
--- 3DCardProject/Assets/01.Scirpts/Stage/StageSO.cs
+++ 3DCardProject/Assets/01.Scirpts/Stage/StageSO.cs
@@ -2,19 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 적에 대한 정보 
-[System.Serializable]
-public class Enemy
-{
-    public Vector2Int vector2Int;
-    public ItemSO item;
-}
-
-[System.Serializable]
-public struct StageData
-{
-    public Enemy[] enemys;
-}
 
 
 [CreateAssetMenu(fileName = "StageSO", menuName = "Scriptable Object/StageSO")]
